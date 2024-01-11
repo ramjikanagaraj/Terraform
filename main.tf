d1718085da8bb0c2f15ef475d0ea847cb27c021a
ramjikanagaraj <ramjikanagaraj@gmail.com>
2024-01-11 10:54:40 +0530

    added variable declartions

---
+++ main.tf
@@ -0,0 +1,155 @@
+// Instance
+
+resource "aws_instance" "public" {
+  ami = var.ami
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.public.id
+  security_groups = [ aws_security_group.demo_vpc_SG.id ]
+
+  tags = {
+    Name = var.first_ins
+  }
+}
+
+resource "aws_instance" "private" {
+  ami = var.ami
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.private.id
+  security_groups = [ aws_security_group.demo_vpc_SG.id ]
+
+  tags = {
+    Name = var.secound_ins
+  }
+}
+
+// VPC
+
+resource "aws_vpc" "demo" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+// public subnet
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.demo.id
+  cidr_block = var.pub_cidr_block
+  availability_zone = var.pub_availability
+  map_public_ip_on_launch = var.pub_sub
+
+  tags = {
+    Name = var.pub_sub_name
+  }
+}
+
+// private subnet
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.demo.id
+  cidr_block = var.pri_cidr_block
+  availability_zone = var.pri_availability
+  map_public_ip_on_launch = var.pri_sub
+
+  tags = {
+    Name = var.pri_sub_name
+  }
+}
+
+// Internetgateway
+
+resource "aws_internet_gateway" "Intgateway" {
+  vpc_id = aws_vpc.demo.id
+
+  tags = {
+    Name = var.Intgat_name
+  }
+}
+
+// public Route table
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.demo.id
+  route {
+    cidr_block = var.pub_route
+    gateway_id = aws_internet_gateway.Intgateway.id
+  }
+
+  tags = {
+    Name = var.Pub_rou_name
+  }
+}
+
+// association
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+// Eip for NAT
+
+resource "aws_eip" "NAT" {
+  instance = null
+  tags = {
+    Name = var.EIP_Name
+  }
+}
+
+// NAT gateway
+
+resource "aws_nat_gateway" "name" {
+  allocation_id = aws_eip.NAT.id
+  subnet_id = aws_subnet.public.id
+  tags = {
+    Name = var.NAT_Name
+  }
+}
+
+// private Route table
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.demo.id
+
+  route {
+    cidr_block = var.pri_route
+    nat_gateway_id = aws_nat_gateway.name.id
+  }
+
+  tags = {
+    Name = var.Pri_rou_name
+  }
+}
+
+// association
+
+resource "aws_route_table_association" "private" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+// security group
+
+resource "aws_security_group" "demo_vpc_SG" {
+  vpc_id      = aws_vpc.demo.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_groups_name
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,107 @@
+//region
+
+variable "region" {
+  type = string
+  description = "give region name"
+}
+
+// ami
+
+variable "ami" {
+    default = "ami-0a0f1259dd1c90938"
+}
+
+// Instance Type
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+// public Instance Name
+
+variable "first_ins" {
+  default = "public"
+}
+
+// private Instance Name
+
+variable "secound_ins" {
+  default = "private"
+}
+
+// VPC cidr_block
+
+variable "cidr_block" {
+    default = "10.0.0.0/16"
+}
+variable "vpc_name" {
+  default = "demo_vpc"
+}
+
+// subnet public 
+
+variable "pub_cidr_block" {
+  default = "10.0.1.0/24"
+}
+variable "pub_availability" {
+  default = "ap-south-1a"
+}
+variable "pub_sub" {
+  default = true
+}
+variable "pub_sub_name" {
+  default = "pub_sub"
+}
+
+// subnet private 
+
+variable "pri_cidr_block" {
+   default = "10.0.2.0/24"
+}
+variable "pri_availability" {
+  default = "ap-south-1b"
+}
+variable "pri_sub" {
+  default = false
+}
+variable "pri_sub_name" {
+  default = "pri_sub"
+}
+
+// Internet gateway
+
+variable "Intgat_name" {
+  default = "Intgate"
+}
+variable "EIP_Name" {
+  default = "Demo EIP"
+}
+
+// NAT 
+
+variable "NAT_Name" {
+  default = "Demo NAT"
+}
+
+// Table route
+
+variable "pub_route" {
+  default = "0.0.0.0/0"
+}
+variable "Pub_rou_name" {
+  default = "pub_route"
+}
+
+variable "pri_route" {
+  default = "0.0.0.0/0" 
+}
+variable "Pri_rou_name" {
+  default = "pri_route"
+}
+
+// security group
+
+variable "security_groups_name" {
+  type        = string
+  description = "Give the security group name"
+  // default = "Demo_VPC_SGs"
+}
