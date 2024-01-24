ba86ae4fe0105fa6c56c3e3c9b7e561495e8f401
ramjikanagaraj <103557528+ramjikanagaraj@users.noreply.github.com>
2024-01-24 14:38:14 +0530

    Delete EC2 directory

--- EC2/EC2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "name" {
-  ami = var.ami
-  instance_type = var.instance_type
-  
-  tags = {
-    Name = var.instance_name
-  }
-}
--- EC2/variable.tfvars
+++
@@ -1,3 +0,0 @@
-ami = "ami-03f4878755434977f"
-instance_type = "t2.micro"
-instance_name = "public"
