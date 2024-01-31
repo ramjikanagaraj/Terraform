030c6f53e4b644a6484070308c7e54698483b7e8
ramjikanagaraj <ramjikanagaraj@gmail.com>
2024-01-31 16:32:05 +0530

    lifecycle

--- Demo Env/local.tf
+++ Demo Env/local.tf
@@ -2,6 +2,16 @@ resource "local_file" "pets" {
   filename = var.filename
   # content  = var.content 
   content = "${random_pet.random.id} that my pet name"
+  file_permission = 0777
+
+  # It will create the new file before deleting the new file
+  lifecycle {
+    create_before_destroy = true
+  }
+  # It will prevent the resource file from the delection 
+    lifecycle {
+    prevent_destroy = false
+  }
 
   # Explicit dependency
   depends_on = [
